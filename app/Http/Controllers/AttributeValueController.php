5254c3c4510fc924b4c106a94585386c9fe6c3cb
c8AwesomeDevs <81355019+c8AwesomeDevs@users.noreply.github.com>
2024-08-08 19:59:47 +0800

    update save attributes function

--- app/Http/Controllers/AttributeValueController.php
+++ app/Http/Controllers/AttributeValueController.php
@@ -27,7 +27,9 @@ use App\Services\SubscriptionService;
 use DateTime;
 use DateTimeZone;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use App\Services\ADHService;
+use Carbon\Carbon;
 
 use App\Http\Controllers\SVGController;
 
@@ -150,7 +152,7 @@ class AttributeValueController extends Controller
         ], 200, [], JSON_NUMERIC_CHECK);
     }
 
-    public function saveAttributeValue(Request $request, CompanyService $companyService, AttributeValueService $attributeValueService, ADHService $adhService, SubscriptionService $subscriptionservice, StripeService $stripeService, $id)
+    public function saveAttributeValue(Request $request, CompanyService $companyService, AttributeValueService $attributeValueService, ADHService $adhService, SubscriptionService $subscriptionService, StripeService $stripeService, $id)
     {
         // Get ADH Configuration
         $adh_config = $adhService->getAdh($request->company_id);
@@ -158,20 +160,65 @@ class AttributeValueController extends Controller
             return response()->json(['error' => 'ADH configuration not found'], 400);
         }
 
-    
         $stream_id = $adh_config[0]['stream_id'] ?? null;
-
-     
         if (!$stream_id) {
             return response()->json(['error' => 'Tenants or Namespace not found in ADH configuration'], 400);
         }
-    
-    
+
         // Validate Request
-        $validated = $request->validate([
+        $this->validateRequest($request);
+
+        $valuesOnRecord = $attributeValueService->countValuesByElementID($id);
+        $limits = $companyService->getCompanyDetails($request->company_id);
+        if ($valuesOnRecord['values'] == $limits["max_datapoints"]) {
+            return response()->json(["error" => "You've reached the limit for 'Datapoint' elements"], 400);
+        }
+
+        // Handle Timestamps
+        $timestamp = Carbon::parse($request->date . ' ' . $request->time)->format('Y-m-d H:i:s');
+        $formattedADHTimestamp = Carbon::parse($request->date . ' ' . $request->time, 'Asia/Manila')->setTimezone('UTC')->format('Y-m-d H:i:s');
+
+        if ($attributeValueService->dataExists($id, $timestamp)) {
+            return response()->json(['error' => 'Data with this timestamp already exists.'], 400);
+        }
+
+        // Calculate Rate of Change
+        $latest = $attributeValueService->getclosestPrev($id, $timestamp);
+        $rateOfChange = $latest ? $this->calculateRateOfChange($request, $latest, $timestamp) : $this->initializeRateOfChange();
+
+        // Prepare Data Payload
+        $summary = $this->getSummary($request);
+        $sqldataPayload = $this->prepareDataPayload($request, $id, $timestamp, $rateOfChange, $summary);
+        $adhdataPayload = $this->prepareDataPayload($request, $id, $formattedADHTimestamp, $rateOfChange, $summary);
+        // Get Access Token and Post Data to ADH
+        $accessToken = $this->getAccessToken();
+        if (!$accessToken) {
+            return response()->json(['error' => 'Failed to retrieve access token'], 400);
+        }
+
+        $adhResponse = $this->postDataToADH($accessToken, $stream_id, $adhdataPayload);
+        if ($adhResponse->failed()) {
+            return response()->json(['error' => 'Failed to post data to ADH', 'details' => $adhResponse->body()], 400);
+        }
+
+        // Save Data Locally
+        $new_value = $attributeValueService->save($sqldataPayload[0]);
+
+        // Update closest next values
+        $this->updateClosestNextValues($attributeValueService, $id, $timestamp, $request);
+
+        // Report usage to Stripe
+        $this->reportUsageToStripe($subscriptionService->getSubscriptions($request->company_id), $stripeService, $request->user_id, $request->company_id);
+
+        return $attributeValueService->getAttributeValues($id, null, null, 'DESC');
+    }
+
+    private function validateRequest(Request $request)
+    {
+        return $request->validate([
             'company_id' => 'nullable',
-            'date' => 'required',
-            'time' => 'required',
+            'date' => 'required|date',
+            'time' => 'required|regex:/^\d{2}:\d{2}(:\d{2})?$/',
             'acetylene' => 'required|numeric',
             'ethylene' => 'required|numeric',
             'methane' => 'required|numeric',
@@ -181,81 +228,47 @@ class AttributeValueController extends Controller
             'carbon_dioxide' => 'required|numeric',
             'oxygen' => 'nullable|numeric',
         ]);
-    
-        $valuesOnRecord = $attributeValueService->countValuesByElementID($id);
-        $limits = $companyService->getCompanyDetails($request->company_id); // company details.. inside here is the limits
-        if ($valuesOnRecord['values'] == $limits["max_datapoints"]) {
-            return response()->json(["error" => "You've reached the limit for 'Datapoint' elements"], 400);
-        }
-        $timestamp = date('Y-m-d H:i:s', strtotime($request->date . ' ' . $request->time));
+    }
 
-        $ADHtimestamp = new DateTime($request->date . ' ' . $request->time, new DateTimeZone('Asia/Manila'));
-        $ADHtimestamp->setTimezone(new DateTimeZone('UTC'));
-        $formattedADHTimestamp = $ADHtimestamp->format('Y-m-d H:i:s');
-    
-        if ($attributeValueService->dataExists($id, $timestamp)) {
-            return response()->json([
-                'message' => 'Data with this timestamp already exists.',
-                'error' => 'Data with this timestamp already exists.'
-            ], 400);
-        }
-    
-        // Calculate Rate of Change
-        $latest = $attributeValueService->getclosestPrev($id, $timestamp);
-        if ($latest) {
-            $date = new DateTime($timestamp);
-            $current_date = new DateTime($latest->timestamp);
-            $difference = $current_date->getTimestamp() - $date->getTimestamp();
-            $days = round(abs($difference / 86400), 4);
-    
-            $acetylene_roc = abs($request->acetylene - $latest->acetylene) / $days;
-            $ethylene_roc = abs($request->ethylene - $latest->ethylene) / $days;
-            $methane_roc = abs($request->methane - $latest->methane) / $days;
-            $ethane_roc = abs($request->ethane - $latest->ethane) / $days;
-            $hydrogen_roc = abs($request->hydrogen - $latest->hydrogen) / $days;
-        } else {
-            $acetylene_roc = $ethylene_roc = $methane_roc = $ethane_roc = $hydrogen_roc = 0;
-        }
-    
-        $summary = $this->getSummary($request);
-    
-        // Get Access Token
-        $url = "https://uswe.datahub.connect.aveva.com/identity/connect/token";
-        $client_id = env('ADH_CLIENT_ID');
-        $client_secret = env('ADH_CLIENT_SECRET');
-    
-        $ADHrefreshToken = Http::asForm()->post($url, [
-            'grant_type' => 'client_credentials',
-            'client_id' => $client_id,
-            'client_secret' => $client_secret
-        ]);
-    
-        if ($ADHrefreshToken->failed()) {
-            return response()->json(['error' => 'Failed to retrieve access token', 'details' => $ADHrefreshToken->body()], 400);
-        }
-        
-        $accessToken = $ADHrefreshToken["access_token"];
-        $tenants = env('ADH_TENANT_ID');
-        $namespace = env('ADH_NAMESPACE');
-        // Prepare URL and Authorization Token for ADH Data POST
-        $url = 'https://auea.datahub.connect.aveva.com/api/v1/Tenants/' . $tenants . '/Namespaces/' . $namespace . '/Streams/' . $stream_id . '/Data';
-        $authorizationToken = 'Bearer ' . $accessToken;
-    
-        $dataPayload = [[
+    private function calculateRateOfChange($request, $latest, $currentTimestamp)
+{
+    $date = new Carbon($currentTimestamp);
+    $current_date = new Carbon($latest->timestamp);
+    $difference = $current_date->diffInSeconds($date);
+    $days = $difference / 86400;
+
+    return [
+        'acetylene_roc' => round(abs($request->acetylene - $latest->acetylene) / $days, 2),
+        'ethylene_roc' => round(abs($request->ethylene - $latest->ethylene) / $days, 2),
+        'methane_roc' => round(abs($request->methane - $latest->methane) / $days, 2),
+        'ethane_roc' => round(abs($request->ethane - $latest->ethane) / $days, 2),
+        'hydrogen_roc' => round(abs($request->hydrogen - $latest->hydrogen) / $days, 2),
+    ];
+}
+
+
+    private function initializeRateOfChange()
+    {
+        return array_fill_keys(['acetylene_roc', 'ethylene_roc', 'methane_roc', 'ethane_roc', 'hydrogen_roc'], 0);
+    }
+
+    private function prepareDataPayload($request, $id, $formattedADHTimestamp, $rateOfChange, $summary)
+    {
+        return [[
             'asset_id' => $request->asset_id,
             'element_id' => $id,
             'company_id' => $request->company_id,
             'timestamp' => $formattedADHTimestamp,
             'acetylene' => $request->acetylene,
-            'acetylene_roc' => $acetylene_roc,
+            'acetylene_roc' => $rateOfChange['acetylene_roc'],
             'ethylene' => $request->ethylene,
-            'ethylene_roc' => $ethylene_roc,
+            'ethylene_roc' => $rateOfChange['ethylene_roc'],
             'methane' => $request->methane,
-            'methane_roc' => $methane_roc,
+            'methane_roc' => $rateOfChange['methane_roc'],
             'ethane' => $request->ethane,
-            'ethane_roc' => $ethane_roc,
+            'ethane_roc' => $rateOfChange['ethane_roc'],
             'hydrogen' => $request->hydrogen,
-            'hydrogen_roc' => $hydrogen_roc,
+            'hydrogen_roc' => $rateOfChange['hydrogen_roc'],
             'oxygen' => $request->oxygen,
             'carbon_monoxide' => $request->carbon_monoxide,
             'carbon_dioxide' => $request->carbon_dioxide,
@@ -276,97 +289,52 @@ class AttributeValueController extends Controller
             'dornenberg' => $summary['dornenberg'],
             'carbon_ratio' => $summary['carbon_ratio'],
             'rogers_ratio' => $summary['rogers_ratio'],
-            'nei' => $summary['nei']
+            'nei' => $summary['nei'],
         ]];
-    
-        // Log the data payload
-        // Log::info('ADH Data Payload: ', $dataPayload);
-    
-        $adhResponse = Http::withHeaders([
+    }
+
+    private function getAccessToken()
+    {
+        return Cache::remember('adh_access_token', 3600, function () {
+            $url = "https://uswe.datahub.connect.aveva.com/identity/connect/token";
+            $client_id = env('ADH_CLIENT_ID');
+            $client_secret = env('ADH_CLIENT_SECRET');
+
+            $response = Http::asForm()->post($url, [
+                'grant_type' => 'client_credentials',
+                'client_id' => $client_id,
+                'client_secret' => $client_secret
+            ]);
+
+            return $response->successful() ? $response['access_token'] : null;
+        });
+    }
+
+    private function postDataToADH($accessToken, $stream_id, $dataPayload)
+    {
+        $url = 'https://auea.datahub.connect.aveva.com/api/v1/Tenants/' . env('ADH_TENANT_ID') . '/Namespaces/' . env('ADH_NAMESPACE') . '/Streams/' . $stream_id . '/Data';
+        $authorizationToken = 'Bearer ' . $accessToken;
+
+        return Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization' => $authorizationToken
         ])->post($url, $dataPayload);
-    
-        if ($adhResponse->failed()) {
-            return response()->json(['error' => 'Failed to post data to ADH', 'details' => $adhResponse->body()], 400);
-        }
-    
-        // Save Data Locally
-        $data = [
-            'element_id' => $id,
-            'company_id' => $request->company_id,
-            'timestamp' => $timestamp,
-            'acetylene' => $request->acetylene,
-            'acetylene_roc' => $acetylene_roc,
-            'ethylene' => $request->ethylene,
-            'ethylene_roc' => $ethylene_roc,
-            'methane' => $request->methane,
-            'methane_roc' => $methane_roc,
-            'ethane' => $request->ethane,
-            'ethane_roc' => $ethane_roc,
-            'hydrogen' => $request->hydrogen,
-            'hydrogen_roc' => $hydrogen_roc,
-            'oxygen' => $request->oxygen,
-            'carbon_monoxide' => $request->carbon_monoxide,
-            'carbon_dioxide' => $request->carbon_dioxide,
-            'tdcg' => $summary['tdcg'],
-            't1' => $summary['t1'],
-            't2' => $summary['t2'],
-            't3_biotemp' => $summary['t3_biotemp'],
-            't3_fr' => $summary['t3_fr'],
-            't3_midel' => $summary['t3_midel'],
-            't3_silicon' => $summary['t3_silicon'],
-            't4' => $summary['t4'],
-            't5' => $summary['t5'],
-            't6' => $summary['t6'],
-            't7' => $summary['t7'],
-            'p1' => $summary['p1'],
-            'p2' => $summary['p2'],
-            'iec_ratio' => $summary['iec_ratio'],
-            'dornenberg' => $summary['dornenberg'],
-            'carbon_ratio' => $summary['carbon_ratio'],
-            'rogers_ratio' => $summary['rogers_ratio'],
-            'nei' => $summary['nei']
-        ];
-    
-        $new_value = $attributeValueService->save($data);
-    
-        // Update closest next values
+    }
+
+    private function updateClosestNextValues(AttributeValueService $attributeValueService, $id, $timestamp, $request)
+    {
         $closestNext = $attributeValueService->getclosestNext($id, $timestamp);
         if ($closestNext) {
-            $date = new DateTime($timestamp);
-            $current_date = new DateTime($closestNext->timestamp);
-            $difference = $current_date->getTimestamp() - $date->getTimestamp();
-            $days = round(abs($difference / 86400), 4);
-    
-            $closestNextacetylene_roc = abs($request->acetylene - $closestNext->acetylene) / $days;
-            $closestNextethylene_roc = abs($request->ethylene - $closestNext->ethylene) / $days;
-            $closestNextmethane_roc = abs($request->methane - $closestNext->methane) / $days;
-            $closestNextethane_roc = abs($request->ethane - $closestNext->ethane) / $days;
-            $closestNexthydrogen_roc = abs($request->hydrogen - $closestNext->hydrogen) / $days;
-    
-            $data2 = [
-                'acetylene_roc' => $closestNextacetylene_roc,
-                'ethylene_roc' => $closestNextethylene_roc,
-                'methane_roc' => $closestNextmethane_roc,
-                'ethane_roc' => $closestNextethane_roc,
-                'hydrogen_roc' => $closestNexthydrogen_roc,
-            ];
-    
-            $nextid = $closestNext->id;
-            $closest_value = $attributeValueService->updateclosestNext($nextid, $data2);
+            $closestRateOfChange = $this->calculateRateOfChange($request, $closestNext, $timestamp);
+            $attributeValueService->updateclosestNext($closestNext->id, $closestRateOfChange);
         }
-    
-        // Report usage to Stripe
-        $subs_type = $subscriptionservice->getSubscriptions($request->company_id);
-        if ($subs_type == "advanced") {
-            // Send usage report to Stripe
-            $usage_quantity = 1; // One datapoint
-            $q = $stripeService->report_usage($request->user_id, $request->company_id, $usage_quantity);
-        }
-    
-        return $attributeValueService->getAttributeValues($id, null, null, 'DESC');
     }
-    
+
+    private function reportUsageToStripe($subscription, StripeService $stripeService, $userId, $companyId)
+    {
+        if ($subscription === "advanced") {
+            $stripeService->report_usage($userId, $companyId, 1);
+        }
+    }
 
 }
